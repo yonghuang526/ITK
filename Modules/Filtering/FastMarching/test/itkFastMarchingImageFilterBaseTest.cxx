5763a1765de2c2f14f7ca2b9d3ddace8c4807cf7
Jon Haitz Legarreta <jhlegarreta@vicomtech.org>
2017-04-25 11:25:00 +0200

    ENH: Improve coverage for itk::FastMarchingImageFilterBase.
    Exercise basic object methods.
    
    Test all Set/Get methods using the TEST_SET_GET_VALUE macro, and all
    On/Off methods corresponding to boolean members usign the
    TEST_SET_GET_BOOLEAN macro.
    
    Use the TRY_EXPECT_NO_EXCEPTION macro to update the filter and save
    tryping the try/catch blocks.
    
    Prefer initialization over assignment in variables.
    
    Use the itkNotUsed macro for test input arguments and hence avoid
    casting argc and argv to void to avoid compiler warnings.
    
    Remove printing the input (speed) image.
    
    Remove the calls to the Get* methods corresponding to base class
    members (specifically: m_SpeedConstant, m_TargetReachedValue, and
    m_NormalizationFactor should be tested for itk::FastMarchingBase in
    itkFastMarchingBaseTest.cxx; and m_StoppingValue and GetCollectPoints
    should be tested for itk::FastMarchingImageFilter in
    itkFastMarchingTest.cxx).
    
    Start comments with capital letters.
    
    Minor style changes (such as white spaces between operators and
    arguments, improvement of type names and error messages).
    
    Change-Id: If0a45d50da63e766dc17a90f68c8c709a41bd0a7

--- Modules/Filtering/FastMarching/test/itkFastMarchingImageFilterBaseTest.cxx
+++ Modules/Filtering/FastMarching/test/itkFastMarchingImageFilterBaseTest.cxx
@@ -18,57 +18,92 @@
 
 #include "itkFastMarchingImageFilterBase.h"
 #include "itkFastMarchingThresholdStoppingCriterion.h"
+#include "itkTestingMacros.h"
 
 template< unsigned int VDimension >
 int FastMarchingImageFilterBase( )
-  {
+{
   using PixelType = float;
 
   using ImageType = itk::Image< PixelType, VDimension >;
   typename ImageType::Pointer input = ImageType::New();
 
-  using FMMType =
+  using FastMarchingImageFilterType =
       itk::FastMarchingImageFilterBase< ImageType, ImageType >;
-  typename FMMType::Pointer fmm = FMMType::New();
-  fmm->SetInput( input );
+  typename FastMarchingImageFilterType::Pointer fastMarchingFilter = 
+      FastMarchingImageFilterType::New();
 
-  bool exception_caught = false;
+  bool overrideOutputInformation = true;
+  TEST_SET_GET_BOOLEAN( fastMarchingFilter, OverrideOutputInformation,
+    overrideOutputInformation );
 
-  try
-    {
-    fmm->Update();
-    }
-  catch( itk::ExceptionObject & excep )
-    {
-    std::cerr << "Exception caught !" << std::endl;
-    std::cerr << excep << std::endl;
-    exception_caught = true;
-    }
+  FastMarchingImageFilterType::OutputSizeType outputSize;
+  outputSize.Fill( 32 );
+  fastMarchingFilter->SetOutputSize( outputSize );
+  TEST_SET_GET_VALUE( outputSize, fastMarchingFilter->GetOutputSize() );
 
-  if( !exception_caught )
-    {
-    std::cout <<"Exception is not caught!" <<std::endl;
-    return EXIT_FAILURE;
-    }
+  FastMarchingImageFilterType::OutputRegionType outputRegion;
+  outputRegion.SetSize( outputSize );
+  fastMarchingFilter->SetOutputRegion( outputRegion );
+  TEST_SET_GET_VALUE( outputRegion, fastMarchingFilter->GetOutputRegion() );
 
-  typename ImageType::Pointer output = fmm->GetOutput();
+  FastMarchingImageFilterType::OutputSpacingType outputSpacing;
+  outputSpacing.Fill( 1.0 );
+  fastMarchingFilter->SetOutputSpacing( outputSpacing );
+  TEST_SET_GET_VALUE( outputSpacing, fastMarchingFilter->GetOutputSpacing() );
+
+  FastMarchingImageFilterType::OutputDirectionType outputDirection;
+  outputDirection.SetIdentity();
+  fastMarchingFilter->SetOutputDirection( outputDirection );
+  TEST_SET_GET_VALUE( outputDirection, fastMarchingFilter->GetOutputDirection() );
+
+  FastMarchingImageFilterType::OutputPointType outputOrigin;
+  outputOrigin.Fill( 0.0 );
+  fastMarchingFilter->SetOutputOrigin( outputOrigin );
+  TEST_SET_GET_VALUE( outputOrigin, fastMarchingFilter->GetOutputOrigin() );
+
+
+  fastMarchingFilter->SetInput( input );
+
+  TRY_EXPECT_NO_EXCEPTION( fastMarchingFilter->Update() );
+
+  typename ImageType::Pointer output = fastMarchingFilter->GetOutput();
 
   return EXIT_SUCCESS;
-  }
+}
 
 
-// ----------------------------------------------------------------------------
 int itkFastMarchingImageFilterBaseTest( int , char * [] )
-  {
+{
+  // Exercise basic object methods
+  // Done outside the helper function in the test because GCC is limited
+  // when calling overloaded base class functions.
+  const unsigned int Dimension = 2;
+  typedef float                               PixelType;
+  typedef itk::Image< PixelType, Dimension >  ImageType;
+
+  typedef itk::FastMarchingImageFilterBase< ImageType, ImageType >
+    FastMarchingImageFilterType;
+
+  FastMarchingImageFilterType::Pointer fastMarchingFilter =
+    FastMarchingImageFilterType::New();
+
+  EXERCISE_BASIC_OBJECT_METHODS( fastMarchingFilter,
+    FastMarchingImageFilterBase, FastMarchingBase );
+
+
   if( FastMarchingImageFilterBase< 2 >() == EXIT_FAILURE )
     {
-    std::cerr << "2D Fails" <<std::endl;
+    std::cerr << "Test failed!" << std::endl;
     return EXIT_FAILURE;
     }
   if( FastMarchingImageFilterBase< 3 >() == EXIT_FAILURE )
     {
-    std::cerr << "3D Fails" <<std::endl;
+    std::cerr << "Test failed!" << std::endl;
     return EXIT_FAILURE;
     }
+
+
+  std::cout << "Test finished." <<std::endl;
   return EXIT_SUCCESS;
-  }
+}
